4ec5e20b9366c91d39ec313822a8a76bcaf23487
Brennen Davis <brennendavis@gmail.com>
2018-09-19 23:33:56 -0600

    Remove Member route. Ombre changed his name

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export const MEMBERS = [
-  { realm: "Kil'jaeden", name: 'Monktis' }, { realm: "Kil'jaeden", name: 'Hyypro' }, { realm: "Kil'jaeden", name: 'Ombre' }, { realm: "Kil'jaeden", name: 'Stwinkedge' },
+  { realm: "Kil'jaeden", name: 'Monktis' }, { realm: "Kil'jaeden", name: 'Hyypro' }, { realm: "Kil'jaeden", name: 'Sevaera' }, { realm: "Kil'jaeden", name: 'Stwinkedge' },
   { realm: "Kil'jaeden", name: 'Darksynergy' }, { realm: "Kil'jaeden", name: 'Troph' }, { realm: "Kil'jaeden", name: 'Boys' }, { realm: "Kil'jaeden", name: 'Çlutch' }, 
   { realm: "Kil'jaeden", name: 'Bun' }, { realm: "Kil'jaeden", name: 'Jargo' }, { realm: "Kil'jaeden", name: 'Salladin' }, { realm: "Kil'jaeden", name: 'Arkturus' }, 
   { realm: "Kil'jaeden", name: 'Muspel' }, { realm: "Uldaman", name: 'Legias' }
