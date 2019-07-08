379be35b9aa5ca8da9c149b02d9003327c143492
Brennen Davis <brennendavis@gmail.com>
2019-07-08 14:59:44 +0000

    Remove blizzard API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  blizz: 't62byf6h6agtpmpkx9w8e9gjbcyd8d6h',
+  blizz: 'Put your blizzard API key here',
   firebase: {
     apiKey: "AIzaSyCf1nZ7Zts2kB8S8at9iaxSeFjXfM85xv4",
     authDomain: "endless-cc3f8.firebaseapp.com",
